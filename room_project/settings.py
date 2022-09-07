eb2c6b646c3819a1adacbc45a26a06c791bd80d6
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-09-07 04:32:03 -1100

    done

--- room_project/settings.py
+++ room_project/settings.py
@@ -144,6 +144,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -151,6 +152,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
