d769d2a1563e74b5a2111d9d65cc75adc52482c8
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-09-07 05:38:25 -1100

    don

--- room_project/settings.py
+++ room_project/settings.py
@@ -154,9 +154,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
