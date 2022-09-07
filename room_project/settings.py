96084a4d597189b3a08b8da79e519e56ff12a6d6
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-09-07 04:40:16 -1100

    done

--- room_project/settings.py
+++ room_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,7 +145,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
